5e442663c1d56dafe9b4eed10090fdc9fd6403ba
Joseph <yeojunjeon42@gmail.com>
2026-03-02 15:30:55 +0900

    add activity schema

---
+++ src/sanity/schemaTypes/activityType.ts
@@ -0,0 +1,13 @@
+import {defineType, defineField} from 'sanity'
+
+export const activityType = defineType({
+  name: 'activity', title: 'Activity', type: 'document', fields: [
+    defineField({
+      name: 'title', title: 'Title', type: 'string'}),
+    defineField({
+      name: 'description', title: 'Description', type: 'text'}),
+    defineField({
+      name: 'image', title: 'Image', type: 'image'}),
+    defineField({
+      name: 'link', title: 'Link', type: 'url'}),
+  ]})
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -3,7 +3,8 @@ import { postType } from './postType'
 import { memberType } from './memberType'
 import { sponsorType } from './sponsorType'
 import { projectType } from './projectType'
+import { activityType } from './activityType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [postType, memberType, sponsorType, projectType],
+  types: [postType, memberType, sponsorType, projectType, activityType],
 }
