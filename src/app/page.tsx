4eb46cef3302017e8a3348575c9c755078d948db
Joseph <yeojunjeon42@gmail.com>
2026-03-02 14:57:15 +0900

    add awesome cursorrules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
 import { T } from "@/components/T";
 
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-dark text-white">
